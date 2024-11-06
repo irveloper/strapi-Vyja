258d04350a6a3b3891ab826ecac86f8a09d66b24
Irving Caamal <irvv.17@gmail.com>
2024-11-06 12:01:24 -0600

    fix(bump-strapi-version): remove i18n plugin

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,49 +1,49 @@
-import type { Schema, Attribute } from '@strapi/strapi';
+import type { Schema, Struct } from '@strapi/strapi';
 
-export interface UiLinks extends Schema.Component {
-  collectionName: 'components_ui_links';
-  info: {
-    displayName: 'links';
-    icon: 'attachment';
-    description: '';
-  };
-  attributes: {
-    label: Attribute.String;
-    url: Attribute.String;
-  };
-}
-
-export interface UiHeroBanner extends Schema.Component {
-  collectionName: 'components_ui_hero_banners';
-  info: {
-    displayName: 'HeroBanner';
-    icon: 'cast';
-  };
-  attributes: {
-    title: Attribute.String;
-    description: Attribute.Text;
-    cta: Attribute.Component<'ui.links'>;
-  };
-}
-
-export interface UiHeading extends Schema.Component {
+export interface UiHeading extends Struct.ComponentSchema {
   collectionName: 'components_ui_headings';
   info: {
     displayName: 'Heading';
     icon: 'strikeThrough';
   };
   attributes: {
-    title: Attribute.String;
-    description: Attribute.Text;
+    description: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
   };
 }
 
-declare module '@strapi/types' {
-  export module Shared {
-    export interface Components {
-      'ui.links': UiLinks;
-      'ui.hero-banner': UiHeroBanner;
+export interface UiHeroBanner extends Struct.ComponentSchema {
+  collectionName: 'components_ui_hero_banners';
+  info: {
+    displayName: 'HeroBanner';
+    icon: 'cast';
+  };
+  attributes: {
+    cta: Schema.Attribute.Component<'ui.links', false>;
+    description: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface UiLinks extends Struct.ComponentSchema {
+  collectionName: 'components_ui_links';
+  info: {
+    description: '';
+    displayName: 'links';
+    icon: 'attachment';
+  };
+  attributes: {
+    label: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
       'ui.heading': UiHeading;
+      'ui.hero-banner': UiHeroBanner;
+      'ui.links': UiLinks;
     }
   }
 }
